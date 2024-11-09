cf16f09b8200271453f0cac6972302ab016d6ec2
bfs-studio <148264699+bfs-studio@users.noreply.github.com>
2024-11-09 11:23:22 +0300

    adding light, camera, material and render

--- bpy-fundamentals-1.py
+++ bpy-fundamentals-1.py
@@ -17,3 +17,31 @@ bpy.ops.mesh.primitive_uv_sphere_add(radius=1, enter_editmode=False, location=(3
 cube = bpy.context.object
 cube.rotation_euler = (0.785, 0.785, 0)  # Rotate 45 degrees on two axes
 cube.scale = (1.5, 1.5, 0.5)  # Make it a flat cuboid
+
+# Adding a material to the cube
+cube_material = bpy.data.materials.new(name="CubeMaterial")
+cube_material.diffuse_color = (1, 0, 0, 1)  # Red color
+cube.data.materials.append(cube_material)
+
+# Adding a material to the sphere
+sphere = bpy.data.objects["Sphere"]
+sphere_material = bpy.data.materials.new(name="SphereMaterial")
+sphere_material.diffuse_color = (0, 0, 1, 1)  # Blue color
+sphere.data.materials.append(sphere_material)
+
+# Add a sun light
+bpy.ops.object.light_add(type='SUN', radius=1, location=(5, 5, 5))
+light = bpy.context.object
+light.data.energy = 5  # Adjust brightness
+
+# Add a camera
+bpy.ops.object.camera_add(location=(8, -8, 5), rotation=(1.109, 0, 0.785))
+bpy.context.scene.camera = bpy.context.object
+
+# Set render resolution
+bpy.context.scene.render.resolution_x = 1920
+bpy.context.scene.render.resolution_y = 1080
+
+# Render the image
+# bpy.ops.render.render(write_still=True)
+# bpy.data.images['Render Result'].save_render(filepath="/tmp/render.png")
