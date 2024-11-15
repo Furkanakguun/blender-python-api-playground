6d217e9c5e977e5b9489dd0bef2eb72c1db25bc6
fakgun <furser06@gmail.com>
2024-11-15 19:16:53 +0300

    randomized sphere continues

--- randomized_spheres_fall.py
+++ randomized_spheres_fall.py
@@ -1,7 +1,7 @@
-import bpy
-import random
-import math
-from math import radians
+import bpy 
+from random import randint
+import random 
+from math import radians 
 
 # Clear Existing Objects
 bpy.ops.object.select_all(action='SELECT')
@@ -10,73 +10,83 @@ bpy.ops.object.delete(use_global=False)
 tk = bpy.data.texts["bco602tk.py"].as_module()
 
 # Create Ground Plane
-bpy.ops.mesh.primitive_plane_add(size=30, location=(0, 0, 0))
+bpy.ops.mesh.primitive_plane_add(size=40, location=(0, 0, -5))
 ground = bpy.context.object
-ground.rotation_euler[0] = -0.385
+ground.rotation_euler[0] = -0.285
 bpy.ops.rigidbody.object_add()
 ground.rigid_body.type = 'PASSIVE'
 
-bpy.ops.mesh.primitive_plane_add(size=30, location=(0, 0, 0))
+bpy.ops.mesh.primitive_plane_add(size=40, location=(0, 0, -5))
 ground = bpy.context.object
-ground.rotation_euler[0] = 0.385
+ground.rotation_euler[0] = 0.285
 bpy.ops.rigidbody.object_add()
 ground.rigid_body.type = 'PASSIVE'
  
- 
-# Number of Spheres and Minimum Distance Between Them
-num_spheres = 25
-min_distance = 1.5
+num_spheres = 100
+positions = [
+    (-1, -1, -1),
+    (-1, -1, -.9),
+    (-1, -1, -.8),
+    (-1, -1, -.7),
+    (-1, -1, -.6),
+    (-1, -1, -.5),
+    (-1, -1, -.4),
+    (-1, -1, -.3),
+    (-1, -1, -.2),
+    (-1, -1, -.1),
+    (-1, -1, 0),
+    (-1, -1, .1),
+    (-1, -1, .2),
+    (-1, -1, .3),
+    (-1, -1, .4),
+    (-1, -1, .5),
+    (-1, -1, .6),
+    (-1, -1, .7),
+    (-1, -1, .8),
+    (-1, -1, .9),
+    (-1, -1, 1)
+]
 
-# Track Positions to Avoid Overlapping
-positions = [] 
+colors = [
+    (1, 0, 0, 1),(0, 1, 0, 1),(0, 0, 1, 1),
+    (1, 1, 0, 1),(1, 0, 1, 1),(0, 1, 1, 1)
+]
 
 for i in range(num_spheres):
-    # Find a non-overlapping position
-    for _ in range(10):  # Try up to 10 times to find a non-overlapping position
-        x, y, z = random.uniform(-5, 5), random.uniform(-5, 5), random.uniform(10, 15)
-        if all(math.dist((x, y, z), pos) >= min_distance for pos in positions):
-            positions.append((x, y, z))
-            break
-    else:
-        continue  # Skip this sphere if a position wasn't found
-    
-    # Create Sphere with Random Position and Scale
-    # Will fail if scene is empty
-    tk.mode("OBJECT")
-    bpy.ops.mesh.primitive_uv_sphere_add(location=(x, y, z))
-    sphere = bpy.context.object
+    tk.create.sphere('sphere')
+    x, y, z = (randint(-10, 10), randint(-10, 10), 12 + randint(-10, 10))
+    tk.sel.translate((x, y, z))
     scale = random.uniform(0.2, 1.0)
-    sphere.scale = (scale, scale, scale)
-    
-    # Add Rigid Body Physics
+    tk.sel.scale((scale, scale, scale))
+
+    alpha = 1
+    color1 = tk.makeMaterial('1', colors[randint(0, 5)], 0.5, 0, 0.1) 
+    color2 = tk.makeMaterial('2', colors[randint(0, 5)], 0.5, 0, 0.1)
+    color3 = tk.makeMaterial('3', colors[randint(0, 5)], 0.5, 0, 0.1)
+    color4 = tk.makeMaterial('4', colors[randint(0, 5)], 0.5, 0, 0.1)
+    color5 = tk.makeMaterial('5', colors[randint(0, 5)], 0.5, 0, 0.1)
+
+    ob = bpy.context.object 
+    me = ob.data 
+    me.materials.append(color1) 
+    me.materials.append(color2)
+    me.materials.append(color3)
+    me.materials.append(color4)
+    me.materials.append(color5)
+
+    tk.mode("EDIT")
+    tk.act.select_by_loc((-1, -1, -1), (1, 1, 1), 'FACE', 'LOCAL') 
+        
+    ob.active_material_index = 1 
+    bpy.ops.object.material_slot_assign()
+
+    for j in positions:
+        tk.act.select_by_loc(j, (1, 1, 1), 'FACE', 'LOCAL')
+        ob.active_material_index = randint(0, 2)
+        bpy.ops.object.material_slot_assign()
+
+    tk.mode("OBJECT")
     bpy.ops.rigidbody.object_add()
-    sphere.rigid_body.type = 'ACTIVE'
-    
-    # Create materials
-    alpha = 0.9
-    red = tk.makeMaterial('Red', (1, 0, 0, alpha), 0.5, 0, 0.1)
-    green = tk.makeMaterial('Green', (0, 1, 0, alpha), 0.5, 0, 0.1)
-    blue = tk.makeMaterial('Blue', (0, 0, 1, alpha), 0.8, 0.8, 0.2)
-    me = sphere.data
-    me.materials.append(red)
-    me.materials.append(blue)
-    me.materials.append(green)
-    tk.mode("EDIT")
-    tk.act.select_by_loc((0, 0, 0), (1, 1, 1), 'FACE', 'GLOBAL')
-    # use second material slot
-    sphere.active_material_index = 1
-    bpy.ops.object.material_slot_assign()
-    tk.mode("OBJECT")
-    tk.sel.rotate_z(radians(90))
-    tk.mode("EDIT")
-    tk.act.select_by_loc((0, 0, 0), (1, 1, 1), 'FACE', 'GLOBAL')
-    sphere.active_material_index = 2
-    bpy.ops.object.material_slot_assign()
-    tk.mode("OBJECT")
-    
-    # Assign the material to the sphere
-    # sphere.data.materials.clear()  # Clear any existing materials
-    # sphere.data.materials.append(red)  # Apply the new material
 
 # Set Gravity and Frame Settings
 bpy.context.scene.gravity = (0, 0, -9.81)
@@ -89,3 +99,14 @@ bpy.context.scene.rigidbody_world.solver_iterations = 10
 
 # Initialize Simulation
 bpy.context.scene.frame_set(1)
+
+
+# for i in range(num_spheres):
+#     # Find a non-overlapping position
+#     for _ in range(10):  # Try up to 10 times to find a non-overlapping position
+#         x, y, z = random.uniform(-5, 5), random.uniform(-5, 5), random.uniform(10, 15)
+#         if all(math.dist((x, y, z), pos) >= min_distance for pos in positions):
+#             positions.append((x, y, z))
+#             break
+#     else:
+#         continue  
